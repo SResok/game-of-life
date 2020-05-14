ee4a2428da360f303c31dcb9bf2ff86546dcba70
SResok <simonresok@hotmail.com>
2020-05-14 20:03:10 +0200

    canvas test

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,37 +1,64 @@
-import React from "react"
+import React, { useRef,useLayoutEffect, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = props => {
+  let windowWidth = Math.floor(window.innerWidth / 100 * 85)
+  let windowHeight = Math.floor((window.innerHeight / 100) * 65)
+  const tileSize = 20
+
+  windowWidth = windowWidth - (windowWidth % tileSize)
+  windowHeight = windowHeight - (windowHeight % tileSize)
+
+  const {
+    width = windowWidth,
+    height = windowHeight,
+    pixelRatio = window.devicePixelRatio,
+  } = props
+
+  const canvas = useRef(null);
+
+  useEffect(() => {
+    const context = canvas.current.getContext("2d")
+
+    context.save()
+    context.scale(pixelRatio, pixelRatio)
+    context.fillStyle = "hsl(0, 0%, 95%)"
+
+    context.fillRect(0, 0, width, height)
+
+    context.strokeStyle = "black"
+    for (let i = 0; i < width; i = i + 20) {
+      for (let j = 0; j < height; j = j + 20) {
+        context.fillStyle = "black"
+        const life = Math.floor(Math.random() * 2)
+        context.beginPath()
+        context.rect(i, j, 20, 20)
+
+        life ? context.fill() : context.stroke()
+
+      }
+    }
+
+    context.restore()
+  })
+
+
+
+  const dw = Math.floor(pixelRatio * width);
+  const dh = Math.floor(pixelRatio * height);
+  const style = { width, height };
+
+
+return (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <div className="gridtest">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-    </div>
+    <h1>CANVAS GAMEOFLIFE</h1>
+    return <canvas ref={canvas} width={dw} height={dh} style={style} />
   </Layout>
 )
+}
 
 export default SecondPage
